cc3d8315c1312c4f806c8d48d92930271f58b3dd
Muhammah Salim <salimmia3745@gmail.com>
2023-12-16 00:31:01 +0600

    patch request done

--- main.go
+++ main.go
@@ -57,11 +57,28 @@ func getTodo(context *gin.Context){
 	context.IndentedJSON(http.StatusOK, todo)
 }
 
+func toggleTodoStatus(context *gin.Context){
+	id := context.Param("id")
+
+	todo, err := getTodoById(id)
+
+	if err != nil{
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
 func main(){
 	router := gin.Default()
 	
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
+	
+	router.PATCH("/todos/:id", toggleTodoStatus)
 
 	router.POST("/todos", addTodos)
 

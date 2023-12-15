31bf5cea41c01ba478300041f8a44290c0ee3bb4
Muhammah Salim <salimmia3745@gmail.com>
2023-12-15 22:55:55 +0600

    initial commit to build rest api using golang

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct{
+	ID			string	`json: "id"`
+	Item		string	`json: "item"`
+	Completed	bool	`json: "completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: true},
+	{ID: "3", Item: "REST API Course Completed", Completed: false},
+}
+
+func getTodos(context *gin.Context){
+	context.IndentedJSON(http.StatusOK, todos);
+}
+
+func main(){
+	router := gin.Default();
+	
+	router.GET("/todos", getTodos);
+
+	router.Run("localhost:8080")
+}
